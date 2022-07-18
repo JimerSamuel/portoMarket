9c3f29c6bc1703601c7921485a75af8730727829
Jimer Samuel Espinoza <ejimersamuel@yahoo.com>
2022-07-18 15:45:04 -0500

    PortoMarket 1.0.7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,64 +1,60 @@
-import React, { useState, useContext } from 'react';
-import '@styles/Header.scss';
+import React, { useContext } from 'react';
 import Menu from '@components/Menu';
 import MyOrder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '../context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import '@styles/Header.scss';
 
 const Header = () => {
-	const [toggle, setToggle] = useState(false);
-	const [toggleOrders, setToggleOrders] = useState(false);
-	const { state } = useContext(AppContext);
-
-	const handleToggle = () => {
-		setToggle(!toggle);
-	}
+	const { state, toggleOrder, toggleMenu } = useContext(AppContext);
 
 	return (
-		<nav>
-			<img src={menu} alt="menu" className="menu" />
-			<div className="navbar-left">
-				<img src={logo} alt="logo" className="nav-logo" />
-				<ul>
-					<li>
-						<a href="/">All</a>
-					</li>
-					<li>
-						<a href="/">Clothes</a>
-					</li>
-					<li>
-						<a href="/">Electronics</a>
-					</li>
-					<li>
-						<a href="/">Furnitures</a>
-					</li>
-					<li>
-						<a href="/">Toys</a>
-					</li>
-					<li>
-						<a href="/">Others</a>
-					</li>
-				</ul>
-			</div>
-			<div className="navbar-right">
-				<ul>
-					<li className="navbar-email" onClick={handleToggle}>
-						platzi@example.com
-					</li>
-					<li
-						className="navbar-shopping-cart"
-						onClick={() => setToggleOrders(!toggleOrders)}
-					>
-						<img src={shoppingCart} alt="shopping cart" />
-						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
-					</li>
-				</ul>
-			</div>
-			{toggle && <Menu />}
-			{toggleOrders && <MyOrder />}
-		</nav>
+		<>
+			<nav>
+				<img src={menu} alt="menu" className="menu" />
+				<div className="navbar-left">
+					<img src={logo} alt="logo" className="nav-logo" />
+					<ul>
+						<li>
+							<a href="/">All</a>
+						</li>
+						<li>
+							<a href="/">Clothes</a>
+						</li>
+						<li>
+							<a href="/">Electronics</a>
+						</li>
+						<li>
+							<a href="/">Furnitures</a>
+						</li>
+						<li>
+							<a href="/">Toys</a>
+						</li>
+						<li>
+							<a href="/">Others</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navbar-right">
+					<ul>
+						<li className="more-clickable-area navbar-email pointer" onClick={() => toggleMenu()}>
+							platzi@example.com
+						</li>
+						<li
+							className="navbar-shopping-cart"
+							onClick={() => toggleOrder()}
+						>
+							<img className="more-clickable-area pointer" src={shoppingCart} alt="shopping cart" />
+							{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+						</li>
+					</ul>
+				</div>
+				{state.menuIsOpen && <Menu />}
+			</nav>
+			{state.orderIsOpen && <MyOrder />}
+		</>
 	);
 }
 
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -5,7 +5,7 @@ import '@styles/MyOrder.scss';
 import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
-	const { state } = useContext(AppContext);
+	const { state, toggleOrder } = useContext(AppContext);
 
 	const sumTotal = () => {
 		const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
@@ -15,23 +15,27 @@ const MyOrder = () => {
 
 	return (
 		<aside className="MyOrder">
-			<div className="title-container">
-				<img src={arrow} alt="arrow" />
-				<p className="title">My order</p>
-			</div>
-			<div className="my-order-content">
-				{state.cart.map(product => (
-					<OrderItem product={product} key={`orderItem-${product.id}`} />
-				))}
-				<div className="order">
-					<p>
-						<span>Total</span>
-					</p>
-					<p>${sumTotal()}</p>
+			<div className="MyOrder-container">
+				<div className="title-container">
+					<img className="more-clickable-area pointer" src={arrow} alt="arrow" onClick={() => toggleOrder()} />
+					<p className="title">My order</p>
 				</div>
-				<button className="primary-button">
-					Checkout
-				</button>
+				<div className="my-order-content">
+					<div className="my-orders">
+						{state.cart.map((product) => (
+							<OrderItem product={product} key={`orderItem-${product.id}`} />
+						))}
+					</div>
+					<div className="order">
+						<p>
+							<span>Total</span>
+						</p>
+						<p>${sumTotal()}</p>
+					</div>
+					<button className="primary-button">
+						Checkout
+					</button>
+				</div>
 			</div>
 		</aside>
 	);
